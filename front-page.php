3fda71d71f5c85be09e481877845b0d1e75fd252
luis2000c <59964726+luis2000c@users.noreply.github.com>
2021-05-31 11:24:25 -0500

    Cambios

--- front-page.php
+++ front-page.php
@@ -1,2 +1,42 @@
-<?php get_header() ?>
-<?php get_footer() ?>
+<?php get_header(); ?>
+
+<main class="h-full">
+    <div class="flex w-full justify-center items-center space-x-10 my-20">
+        <div class="w-1/12 rounded-full hover:shadow-2xl transition">
+            <img
+                class="rounded-full w-full"
+                src="<?php echo get_template_directory_uri() ?>/assets/img/1.jfif" 
+                alt="imagen de perfil de Gio"
+            >
+        </div>
+        <div class="w-6/12">
+            <p class="select-none">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem iure pariatur autem repudiandae, a dolores, temporibus ipsum vero ut nulla distinctio rem consequatur? Laboriosam suscipit, ullam placeat possimus perferendis veniam? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam, debitis quisquam corrupti animi quos quae explicabo nostrum. Aut recusandae atque porro ipsam cumque minus quod, cum ipsa repudiandae voluptas officiis?</p>
+        </div>
+    </div>
+    <div class="flex h-52 space-x-10 mt-10 w-8/12 mx-auto">
+        <div class="w-full h-full border-2 hover:shadow-lg transition rounded-b-lg">
+            <img class="h-4/5 w-full" src="<?php echo get_template_directory_uri()?>/assets/img/2.jpg" alt="">
+            <p class="h-1/5 text-center leading-9 select-none">Herramienta</p>
+        </div>
+        <div class="w-full h-full border-2 hover:shadow-lg transition rounded-b-lg">
+            <img class="h-4/5 w-full" src="<?php echo get_template_directory_uri()?>/assets/img/3.jpg" alt="">
+            <p class="h-1/5 text-center leading-9 select-none">Herramienta</p>
+        </div>
+        <div class="w-full h-full border-2 hover:shadow-lg transition rounded-b-lg">
+            <img class="h-4/5 w-full" src="<?php echo get_template_directory_uri()?>/assets/img/4.jpg" alt="">
+            <p class="h-1/5 text-center leading-9 select-none">Herramienta</p>
+        </div>
+        <div class="w-full h-full border-2 hover:shadow-lg transition rounded-b-lg">
+            <img class="h-4/5 w-full" src="<?php echo get_template_directory_uri()?>/assets/img/5.jpg" alt="">
+            <p class="h-1/5 text-center leading-9 select-none">Herramienta</p>
+        </div>
+    </div>
+    <div class="flex w-full justify-center mt-10 text-justify">
+        <p class="w-8/12 select-none">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum dicta facilis praesentium fuga dolor in, distinctio rerum possimus eligendi vitae animi suscipit, assumenda eius exercitationem voluptatibus itaque. Iusto, eligendi sunt!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo at porro esse molestiae animi natus dolore qui, suscipit libero eius perspiciatis sapiente nesciunt adipisci blanditiis id a ipsa perferendis error.
+        </p>
+    </div>
+</main>
+
+<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="flex w-full h-full">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head()?>
+    <?php wp_head() ?>
 </head>
-<body>
-    
+<body class="flex flex-col w-full h-full">
+    <header>
+        <nav class="flex items-center justify-between p-10 bg-gray-50">
+            <div class="w-1/12">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/6.png" alt="logo giothcode">
+            </div>
+            <div class="text-2xl">
+                Analista de sistemas
+            </div>
+        </nav>
+    </header>
